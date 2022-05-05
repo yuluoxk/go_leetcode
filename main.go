221137342b7f8c405a36b123d7247960c00b5709
yuluo <33423099+yuluoxk@users.noreply.github.com>
2022-05-05 10:32:55 +0800

    new

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 169
+	a := []int{1, 2, 2, 3, 4, 2, 2, 2}
+	b := majorityElement(a)
+	fmt.Println(b)
+}
+
+// 169 多数元素
+func majorityElement(nums []int) int {
+	var endNum, count int = nums[0], 1
+	for _, value := range nums {
+		if endNum == value {
+			count++
+		} else {
+			count--
+			if count == 0 {
+				endNum = value
+				count = 1
+			}
+		}
+	}
+	return endNum
+}
