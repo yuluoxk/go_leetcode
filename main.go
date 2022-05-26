9fcb0fea8686d991c34a665bc75c59c86c711146
yuluo <33423099+yuluoxk@users.noreply.github.com>
2022-05-26 14:31:33 +0800

    add 236. 二叉树的最近公共祖先

--- main.go
+++ main.go
@@ -55,6 +55,60 @@ func main() {
 	fmt.Println(b)
 }
 
+//236. 二叉树的最近公共祖先
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	parent := map[int]*TreeNode{}
+	visited := map[int]bool{}
+
+	var dfs func(node *TreeNode)
+	dfs = func(r *TreeNode) {
+		if r == nil {
+			return
+		}
+		if r.Left != nil {
+			parent[r.Left.Val] = r
+			dfs(r.Left)
+		}
+		if r.Right != nil {
+			parent[r.Right.Val] = r
+			dfs(r.Right)
+		}
+	}
+	dfs(root)
+
+	for p != nil {
+		visited[p.Val] = true
+		p = parent[p.Val]
+	}
+
+	for q != nil {
+		if visited[q.Val] == true {
+			return q
+		}
+		q = parent[q.Val]
+	}
+	return nil
+}
+
+//236. 二叉树的最近公共祖先(递归)
+func lowestCommonAncestor1(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor1(root.Left, p, q)
+	right := lowestCommonAncestor1(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil {
+		return right
+	}
+	return left
+}
+
 //206. 反转链表 —迭代
 func reverseList1(head *ListNode) *ListNode {
 	var prev *ListNode = nil
