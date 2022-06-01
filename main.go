3d14bb5a8318cdeb47db120aae700a15e6220e4f
yuluo <33423099+yuluoxk@users.noreply.github.com>
2022-06-01 14:08:26 +0800

    add 141. 环形链表 快慢指针 add 141. 环形链表 哈希表

--- main.go
+++ main.go
@@ -54,6 +54,35 @@ func main() {
 	moveZeroes(a)
 }
 
+//141. 环形链表 快慢指针
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow, fast := head, head.Next
+	for fast != slow {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}
+
+//141. 环形链表 哈希表
+func hasCycle1(head *ListNode) bool {
+	seen := map[*ListNode]struct{}{}
+	for head != nil {
+		if _, ok := seen[head]; ok {
+			return true
+		}
+		seen[head] = struct{}{}
+		head = head.Next
+	}
+	return false
+}
+
 //283. 移动零 双指针
 func moveZeroes(nums []int) {
 	left, right, n := 0, 0, len(nums)
